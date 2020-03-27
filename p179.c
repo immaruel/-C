7030dd993303a6bcf1059698b000f17f5cfc39f5
immaruel <58267660+immaruel@users.noreply.github.com>
2020-03-28 00:13:05 +0900

    2020.03.28

---
+++ p179.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    char c;
+    printf("정수입력 : ");
+    scanf("%d", &num);
+
+    while (scanf("%c", &c))
+    {
+        if(num < 0)
+            printf("입력된 값은 0보다 작다. \n");
+        if(num > 0)
+            printf("입력된 값은 0보다 크다. \n");
+        if(num == 0)
+            printf("입력된 값은 0이다. \n");
+    }
+}
